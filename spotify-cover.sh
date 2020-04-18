766aa0c2408c94c880796673ceac31e5ebebbec1
Johannes Kazantzidis <johannek@lap5cg00815rc.se.axis.com>
2020-04-18 18:39:13 +0200

    moving to private laptop

--- spotify-cover.sh
+++ spotify-cover.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+touch ~/.cache/wetch/spotify_cover_id
 id_current=$(cat ~/.cache/wetch/spotify_cover_id)
 
 id_new=`dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep spotify:track | cut -d ":" -f3 | cut -d '"' -f1 | sed -n '1p'`
@@ -10,21 +10,13 @@ if [ "$id_new" != "$id_current" ]; then
 	cover=`ls ~/.cache/wetch/ | grep $id_new`
 
 	if [ "$cover" == "" ]; then
-	    imgurl=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | egrep -A 1 "artUrl" | egrep -v "artUrl"| cut -b 44- | cut -d '"' -f 1`
+	    imgid=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -Eo "[[:alnum:]]{30,}"`
+        imgurl="https://d3rt1990lpmkn.cloudfront.net/640/${imgid}"
 
         wget -q -O ~/.cache/wetch/current.jpg $imgurl &> /dev/null
         find ~/.cache/wetch/ -name "current.jpg" -exec mogrify -format png {} \;
-	    # rm wget-log #wget-logs are accumulated otherwise
-	    # cover=`ls ~/.cache/wetch | grep $id_new`
+
 	fi
 
-	# if [ "$cover" != "" ]; then
-	# 	cp ~/.cache/wetch/$cover ~/.cache/wetch/current.jpg
-	# else
-	# 	cp ~/.cache/wetch/empty.jpg ~/.cache/wetch/current.jpg
-	# fi
 	echo $id_new > ~/.cache/wetch/spotify_cover_id
 fi
-
-
-wget -q -O ~/test.jpg `dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | egrep -A 1 "artUrl" | egrep -v "artUrl"| cut -b 44- | cut -d '"' -f 1` &> /dev/null
--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 id_new=`dbus-send --print-reply --session --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep spotify:track | cut -d ":" -f3 | cut -d '"' -f1 | sed -n '1p'`
 
-imgurl=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata'` # | egrep -A 1 "artUrl" | egrep -v "artUrl"| cut -b 44- | cut -d '"' -f 1`
+imgid=`dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.freedesktop.DBus.Properties.Get string:'org.mpris.MediaPlayer2.Player' string:'Metadata' | grep -Eo "[[:alnum:]]{30,}"` #| egrep -A 1 "artUrl" | egrep -v "artUrl"| cut -b 44- | cut -d '"' -f 1`
+imgurl="https://d3rt1990lpmkn.cloudfront.net/640/${imgid}"
 echo ${imgurl}
 # wget ${imgurl} -O ./test.jpg
+
+ # https://d3rt1990lpmkn.cloudfront.net/640/f15552e72e1fcf02484d94553a7e7cd98049361a
