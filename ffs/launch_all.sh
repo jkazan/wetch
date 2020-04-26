cf51062d6dc9e73532ddd6c54f364ced120c7acd
Johannes Kazantzidis <j.kazantzidis13@alumni.imperial.ac.uk>
2020-04-26 16:20:34 +0200

    added load

--- ffs/launch_all.sh
+++ ffs/launch_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #sleep 10
-conky -dq -c ~/wetch/conky-cards/time_rc
-conky -dq -c ~/wetch/conky-cards/misc_rc
-conky -dq -c ~/wetch/conky-cards/spotify_rc
-conky -dq -c ~/wetch/conky-cards/load_rc
-# conky -dq -c ~/wetch/conky-cards/system_rc
-# conky -dq -c ~/wetch/conky-cards/processes_rc
-# conky -dq -c ~/wetch/conky-cards/memory_rc
-# conky -dq -c ~/wetch/conky-cards/filesystem_rc
-# conky -dq -c ~/wetch/conky-cards/network_rc
-# conky -dq -c ~/wetch/conky-cards/fortune_rc
-# conky -dq -c ~/wetch/conky-cards/mediaplayer_rc
+conky -dq -c ~/wetch/ffs/time_rc
+conky -dq -c ~/wetch/ffs/misc_rc
+# conky -dq -c ~/wetch/ffs/spotify_rc
+conky -dq -c ~/wetch/ffs/load_rc
+# conky -dq -c ~/wetch/ffs/system_rc
+# conky -dq -c ~/wetch/ffs/processes_rc
+# conky -dq -c ~/wetch/ffs/memory_rc
+# conky -dq -c ~/wetch/ffs/filesystem_rc
+# conky -dq -c ~/wetch/ffs/network_rc
+# conky -dq -c ~/wetch/ffs/fortune_rc
+# conky -dq -c ~/wetch/ffs/mediaplayer_rc
--- ffs/load.lua
+++ ffs/load.lua
@@ -34,7 +34,7 @@ function conky_main()
 end
 
 function draw_load()
-   local x=20
+   local x=30
 
    local thick = 15
 
@@ -47,7 +47,8 @@ function draw_load()
 
    circleFill(cr, x+fs*3, y+r-fs/4, r, thick, 0, 240, "100", 100, c[1], c[2], c[3], 0.2)
    circleFill(cr, x+fs*3, y+r-fs/4, r, thick, 0, 240, cpu_usage, 100, c[1], c[2], c[3], 0.5)
-   jprint(cr, "cpu", x, y, fs, c[1], c[2], c[3], 1, font_n)
+   jprint(cr, "cpu", 0, y, fs, c[1], c[2], c[3], 1, font_n)
+   jprint(cr, cpu_usage, fs*2.5, y, fs, c[1], c[2], c[3], 1, font_n)
 
    local start=40
    local length=190/4 - 5
@@ -84,7 +85,8 @@ function draw_load()
 
    circleFill(cr, x+fs*3, y+r-fs/4, r, thick, 0, 240, "100", 100, c[1], c[2], c[3], 0.2)
    circleFill(cr, x+fs*3, y+r-fs/4, r, thick, 0, 240, ram_usage, 100, c[1], c[2], c[3], 0.5)
-   jprint(cr, "ram", x, y, fs, c[1], c[2], c[3], 1, font_n)
+   jprint(cr, "ram", 0, y, fs, c[1], c[2], c[3], 1, font_n)
+   jprint(cr, ram_usage, fs*2.5, y, fs, c[1], c[2], c[3], 1, font_n)
 
    -- root
    local r = 40
@@ -94,7 +96,8 @@ function draw_load()
 
    circleFill(cr, x+fs*3, y+r-fs/4, r, thick, 0, 240, "100", 100, c[1], c[2], c[3], 0.2)
    circleFill(cr, x+fs*3, y+r-fs/4, r, thick, 0, 240, root_usage, 100, c[1], c[2], c[3], 0.5)
-   jprint(cr, "root", x-11, y, fs, c[1], c[2], c[3], 1, font_n)
+   jprint(cr, "root", 0, y, fs, c[1], c[2], c[3], 1, font_n)
+   jprint(cr, root_usage, fs*2.5, y, fs, c[1], c[2], c[3], 1, font_n)
 
 
    -- swap
@@ -105,7 +108,8 @@ function draw_load()
 
    circleFill(cr, x+fs*3, y+r-fs/4, r, thick, 0, 240, "100", 100, c[1], c[2], c[3], 0.2)
    circleFill(cr, x+fs*3, y+r-fs/4, r, thick, 0, 240, swap_usage, 100, c[1], c[2], c[3], 0.5)
-   jprint(cr, "swap", x-11, y, fs, c[1], c[2], c[3], 1, font_n)
+   jprint(cr, "swap", 0, y, fs, c[1], c[2], c[3], 1, font_n)
+   jprint(cr, swap_usage, fs*2.5, y, fs, c[1], c[2], c[3], 1, font_n)
 end
 
 -------------------------------------
