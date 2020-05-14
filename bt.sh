c84d8a2d09cc3dbbe9e3d57187c7ca739a4161b7
Johannes Kazantzidis <johannes.kazantzidis@axis.com>
2020-05-14 12:25:52 +0200

    cleanup

--- bt.sh
+++ bt.sh
@@ -1,6 +1,24 @@
-# if [[ `hcitool con | grep -o 'SLAVE AUTH'` ]]; then
-#     echo connected
-# else
-#     echo disconnected
-# fi
-echo hej
+#!/bin/bash
+targets=($(hcitool con | grep -oE "([[:alnum:]]+:){5}[[:alnum:]]+"))
+first=true
+
+for i in "${targets[@]}"
+do
+    name=`hcitool name $i`
+    if [ -z "$name" ]; then
+        if [ $i == "FE:C0:47:E8:C1:7D" ]; then
+            name="MX Master 2"
+	elif [ $i == "2C:41:A1:52:B9:27" ]; then
+            name="Bose Free Soundsport"
+        else
+            name=$i
+        fi
+    fi
+
+    if [ $first = true ]; then
+        first=false
+        echo "Bluetooth: $name"
+    else
+        echo "           $name"
+    fi
+done
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-chmod +x wetch
-mkdir ~/.fonts
-cp poiret-one/PoiretOne-Regular.ttf /home/$USER/.fonts/
-fc-cache -fv
