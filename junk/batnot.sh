b68b2a0c4ada5f8eb6ff0931aac1fd3fa63a298e
Johannes Kazantzidis <johannes.kazantzidis@axis.com>
2021-09-10 22:46:03 +0200

    Cleanup

--- junk/batnot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-battery=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 \
-              | grep "percentage:[[:space:]]*[0-9]*%" | grep -o [0-9]*)
-
-if [[ $battery -le 15 ]] ; then
-    notify-send -i ~/wetch/batlow.svg wetch "Battery $battery%"
-fi
